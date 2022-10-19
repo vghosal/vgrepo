0322748cde39017a7435246dcf2471f9fa7d550b
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-18 21:37:26 -0400

    testing

--- main.tf
+++ main.tf
@@ -1,24 +1,23 @@
-
 provider "aws" {
   # Configuration options
-  region = "ap-southeast-1"
+  region = "us-east-1"
   access_key = "AKIAYAAWXR7UDVL6RYC6"
   secret_key = "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"
 }
 
-data "aws_ami" "app_ami"{
-most_recent=true
-owners=["amazon"]
+# data "aws_ami" "app_ami"{
+# most_recent=true
+# owners=["amazon"]
 
-filter {
-    name= "name"
-    values=["amzn2-ami-hvm*"]
-}
-}
+# filter {
+#     name= "name"
+#     values=["amzn2-ami-hvm*"]
+# }
+# }
 
 
 resource "aws_instance" "webserver"{
   instance_type = "t2.micro"
-  ami= data.aws_ami.app_ami.id
+  ami= "testing"
   }
 
