b3299ccfdac3ebf7f89744d5578efae308b0dbea
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-18 21:40:52 -0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,19 @@ provider "aws" {
   secret_key = "PgOXeQzzBySpZPe2J9qdpTyvTUFHeDbTfJSVxkra"
 }
 
-# data "aws_ami" "app_ami"{
-# most_recent=true
-# owners=["amazon"]
+data "aws_ami" "app_ami"{
+most_recent=true
+owners=["amazon"]
 
-# filter {
-#     name= "name"
-#     values=["amzn2-ami-hvm*"]
-# }
-# }
+filter {
+    name= "name"
+    values=["amzn2-ami-hvm*"]
+}
+}
 
 
 resource "aws_instance" "webserver"{
   instance_type = "t2.micro"
-  ami= "testing"
+  ami= data.aws_ami.app_ami.id
   }
 
