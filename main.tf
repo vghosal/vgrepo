e745e988e6a86ae60411f2d09a5f640dd7dadd20
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-19 21:19:32 -0400

    test format

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
   # Configuration options
   region = "us-east-1"
-  
+
 }
 
-data "aws_ami" "app_ami"{
-most_recent=true
-owners=["amazon"]
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
 
-filter {
-    name= "name"
-    values=["amzn2-ami-hvm*"]
-}
-}
-
-
-resource "aws_instance" "webserver"{
-  instance_type = "t2.micro"
-  ami= data.aws_ami.app_ami.id
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
   }
+}
+
+
+resource "aws_instance" "webserver" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.app_ami.id
+}
 
