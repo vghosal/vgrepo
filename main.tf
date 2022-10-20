4db8b35f64c5d2d17ec624de882f53deac166a53
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-19 21:44:03 -0400

    test sg

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   # Configuration options
   region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
 
 }
 
